9a38acec08ce2d1dd71c8d4ad8a5a2cfb2f7fc2f
Ritik <iritikdev@gmail.com>
2022-08-21 14:10:59 +0530

    ...

--- src/styles.js
+++ src/styles.js
@@ -11,7 +11,7 @@ const styles = {
   flexStart: "flex justify-center items-start",
 
   paddingX: "sm:px-16 px-6",
-  paddingY: "sm:py-28 py-20",
+  paddingY: "sm:py-16 py-16",
   padding: "sm:px-16 px-6 sm:py-12 py-4",
 
   marginX: "sm:mx-16 mx-6",
